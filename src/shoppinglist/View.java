a1f897dd13de2d10bfdd78b1c368c7c2b3fcc938
ArneKreikebaum <Arnekreikebaum@gmail.com>
2023-01-16 19:02:50 +0100

    Update View.java added the Hinzufügen and Bearbeiten GUI

--- src/shoppinglist/View.java
+++ src/shoppinglist/View.java
@@ -92,48 +92,135 @@ public class View implements ActionListener{
     
 // addGUI Declarations
     
-    private JFrame addframe;
+    private JFrame addFrame;
     private JPanel addPanel;
-    private JTextArea a;
-    private JTextArea b;
-    private JTextArea c;
-    private JLabel aL;
-    private JLabel bL;
-    private JLabel cL;
+    
+    private JTextArea addinputAnzahl;
+    private JLabel addlabelAnzahl;
+    
+    private JTextArea addinputName;
+    private JLabel addlabelName;
+    
+    private JTextArea addinputPreis;
+    private JLabel addlabelPreis;
+    
     private JButton addItemButton;
+    private JButton addcancelButton;
     
     public void addGUI() {
         
-        addframe = new JFrame();
-        addframe.setTitle("Fügen sie einen Artikel hinzu!");
-        addframe.setSize(500,500);
-        
+        addFrame = new JFrame();
         addPanel = new JPanel();
-        addframe.add(addPanel);
+        
+        addFrame.setTitle("Fügen sie einen Artikel hinzu!");
+        addFrame.setLocation(new Point(500, 300));
+        addFrame.add(addPanel);
+        addFrame.setSize(new Dimension(313, 300));
+        addFrame.setResizable(false);
+        addFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        
         addPanel.setLayout(null);
         
-        aL = new JLabel("Anzahl: ");
-        aL.setBounds(10,80,20,5);
-        addPanel.add(aL);
+        addlabelAnzahl = new JLabel("Anzahl: ");
+        addlabelAnzahl.setBounds(50, 7, 70, 20);
         
-        a = new JTextArea();
-        a.setBounds(100,20,165,25);
-        addPanel.add(a);
+        addinputAnzahl = new JTextArea();
+        addinputAnzahl.setBounds(50, 27, 193, 28);
         
-        b = new JTextArea();
-        c = new JTextArea();
+        addlabelName = new JLabel("Artikel: ");
+        addlabelName.setBounds(50, 55, 70, 20);
         
-        bL = new JLabel("Artikel: ");
-        cL = new JLabel("Preis: ");
+        addinputName = new JTextArea();
+        addinputName.setBounds(50, 75, 193, 28);
+        
+        
+        addlabelPreis = new JLabel("Preis: ");
+        addlabelPreis.setBounds(50, 103, 90, 20);
+        
+        addinputPreis = new JTextArea();
+        addinputPreis.setBounds(50, 123, 193, 28);
         
         addItemButton = new JButton("Hinzufügen");
-            
-        addframe.setVisible(true);
+        addItemButton.setBounds(50, 160, 193, 28);
         
+        addcancelButton = new JButton("Abbrechen");
+        addcancelButton.setBounds(50, 195, 193, 28);
+        
+        addPanel.add(addlabelAnzahl);
+        addPanel.add(addinputAnzahl);
+        addPanel.add(addlabelName);
+        addPanel.add(addinputName);
+        addPanel.add(addlabelPreis);
+        addPanel.add(addinputPreis);
+        addPanel.add(addItemButton);
+        addPanel.add(addcancelButton);
+        
+        addFrame.setVisible(true);
     }
+    
+    private JFrame changeFrame;
+    private JPanel changePanel;
+    
+    private JTextArea changeinputAnzahl;
+    private JLabel changelabelAnzahl;
+    
+    private JTextArea changeinputName;
+    private JLabel changelabelName;
+    
+    private JTextArea changeinputPreis;
+    private JLabel changelabelPreis;
+    
+    private JButton changeItemButton;
+    private JButton changecancelButton;
     
     public void changeGUI() {
         
+        changeFrame = new JFrame();
+        changePanel = new JPanel();
+        
+        changeFrame.setTitle("Bearbeiten.");
+        changeFrame.setLocation(new Point(500, 300));
+        changeFrame.add(changePanel);
+        changeFrame.setSize(new Dimension(313, 300));
+        changeFrame.setResizable(false);
+        changeFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        
+        changePanel.setLayout(null);
+        
+        changelabelAnzahl = new JLabel("Anzahl: ");
+        changelabelAnzahl.setBounds(50, 7, 70, 20);
+        
+        changeinputAnzahl = new JTextArea();
+        changeinputAnzahl.setBounds(50, 27, 193, 28);
+        
+        changelabelName = new JLabel("Artikel: ");
+        changelabelName.setBounds(50, 55, 70, 20);
+        
+        changeinputName = new JTextArea();
+        changeinputName.setBounds(50, 75, 193, 28);
+        
+        changelabelPreis = new JLabel("Preis: ");
+        changelabelPreis.setBounds(50, 103, 90, 20);
+        
+        changeinputPreis = new JTextArea();
+        changeinputPreis.setBounds(50, 123, 193, 28);
+        
+        changeItemButton = new JButton("Bearbeiten");
+        changeItemButton.setBounds(50, 160, 193, 28);
+        
+        changecancelButton = new JButton("Abbrechen");
+        changecancelButton.setBounds(50, 195, 193, 28);
+        
+        changePanel.add(changelabelAnzahl);
+        changePanel.add(changeinputAnzahl);
+        changePanel.add(changelabelName);
+        changePanel.add(changeinputName);
+        changePanel.add(changelabelPreis);
+        changePanel.add(changeinputPreis);
+        changePanel.add(changeItemButton);
+        changePanel.add(changecancelButton);
+        
+        changeFrame.setVisible(true);
     }
     
     @Override
@@ -153,7 +240,7 @@ public class View implements ActionListener{
         }
         
         if(button == changeButton){
-            // change()
+            changeGUI();
             System.out.println("CHANGE");
         }
         
